890f1e1ed33d94d012804b0b4c78403e681cb4e5
Benyahia Lina <83450184+Lina238@users.noreply.github.com>
2023-01-05 08:35:55 +0100

    Update scrape_results.py

--- scrape_results.py
+++ scrape_results.py
@@ -1,93 +1,88 @@
 import requests
 from typing  import List
 import pandas
+import re
 import csv
+from urlextract import URLExtract
 from urllib.request import urlopen
+from selenium import webdriver
 from django.core.management.base import BaseCommand
 from bs4 import BeautifulSoup
 class Command(BaseCommand):
     help='load data'
     def handle(self,*args, **kwargs):
-     urls=self.construct_urls()
-     listfinale=[]  
-     lis=[]    
-     dic={'Adresse':'','Surface': '','Prix': '','Text': '','Insérée le': '',}
-     l = []
-     for url in urls:
-      q=[] 
-      t1= [] 
-      t4=[]
-      t= []
-      r=requests.get( url)
-      souup=BeautifulSoup(r.text,'html.parser')
-      tb=souup.find('table')  
-      for tr in  tb.find_all('tr'):
-           for td in tr.find_all('td'):
+           elements=[]
+           urls=self.construct_urls()
+           print(len(urls))
+           extractor = URLExtract()
+           fld_name=["titre","niveau","discription","image","mode", "nom", "prix", "lieu"]
+           for url in urls:
+                page=requests.get(url)
+                scp=BeautifulSoup(page.content,'html.parser')
           
-             for tab in td.find_all('table'):
-               for tr in  tab.find_all('tr'):
-                for td in tr.find_all('td'):
+                titre=scp.find_all('h1',class_='anntot')[0].text.strip()                
+                discription=scp.find_all('h3')[0].text.strip() 
+                divg = scp.find('div',class_='img_anntot',style=True)             
+                imgurl = extractor.find_urls(str(divg))
+                nom=scp.find ( 'div',style=lambda value: value and 'font-size:1.2rem' in value ).text.strip()               
+                prix=scp.find ( 'p',style=lambda value: value and 'font-size:1.5rem' in value ).text.strip()
+                mode=scp.find ( 'div',style=lambda value: value and 'margin-top:2' in value ).text.strip()
                 
-                 for tab in td.find_all('table',class_="da_rub_cadre"):
-                     for tab in td.find_all('table',class_="da_rub_cadre"):
-                        for tr in  tab.find_all('tr'):
-                            for td in tr.find_all('td',class_="da_label_field"):
-                             if td.text  not in  t:
-                                 t.append(td.text)
-                             
-                            
-                            for td in tr.find_all('td',class_="da_field_text"):
-                               t1.append(td.text.strip()) 
-                               
-                              
-      t3=[t1[x:x+7] for x in range(0, len(t1),7)]  
-        
-      for ind in t3:
-                   if ind  not in  t4:
-                     t4.append(t3)
-              
-                    
-      for a in range(1,7,1):#pour les champs    
-                       
-                          q.append(t4[0][0][a])
-        
-            
-     l.append(q)       
-     
+                lieu= scp.find('p',style=lambda value: value and 'margin-bottom:0' in value and 'padding-bottom:0' in value ).text.strip() 
+                lieu=lieu.replace( "Domiciliation :", "")
+                t=re.compile(r'\s+')
+                lieu=re.sub(t, '', lieu)
+                lieu = ''.join([i for i in lieu if not i.isdigit()])
+                lieu= lieu.replace('(','').replace(')','')  
+                
+                dict={
+                  "titre":titre,
+                  "niveau":"supérieur",
+                  "discription":discription,
+                  "image":imgurl,
+                   "mode":mode,
+                  "nom":nom,
+                  "prix":prix,
+                  # "possibilité du paiment":paimentpos,
+                  "lieu":lieu,                  
+                }
+                elements.append(dict)    
+           with open('infos.csv', 'a', encoding="utf-8") as csvfile:
+                   writer = csv.DictWriter(csvfile,fieldnames=fld_name)
+                   writer.writeheader()
+                   writer.writerows(elements)
 
-     
-        #   dic={'Adresse':'','Surface': '','Prix': '','Text': '','Insérée le': '',}
-        
-     for l in l: 
-    
-       dic['Adresse']=l[0]
-       dic['Surface']=l[1]
-       dic['Prix']=l[2]
-       dic['Text']=l[3]
-       dic['Insérée le']=l[4]
-       lis.append(dic)
-     listfinale.append(lis)  
-     print(listfinale)                           
+                
     def construct_urls(self) ->List[str]:
          
-         page= requests.get("http://www.annonce-algerie.com/AnnoncesImmobilier.asp")
-
+         page= requests.get("https://dz.professeurparticulier.com/marech.php?ap=9&cat=1&pp=1&pays=Alg%E9rie&nivmax=10&prox=20&go=1")
+         
          soupi =BeautifulSoup (page.content, 'html.parser') 
          links= soupi.find_all('a', href=True)
-
+        
          details_annonces=[]
 
          for link in links:
-
-
-          if "DetailsAnnonceImmobilier" in link['href']: 
-              details_annonces.append(f" {'http://www.annonce-algerie.com/'}{link['href']}")
+          if "annonce-professeur" in link['href']:               
+              details_annonces.append(f" {'https://dz.professeurparticulier.com'}{link['href']}")
 
 
          
          return details_annonces
-
-
-
-
-
+    def LinkPage(self) ->List[str]:
+          global result
+          
+          page=requests.get("https://dz.professeurparticulier.com/marech.php?ap=9&cat=1&pp=1&pays=Alg%E9rie&nivmax=10&prox=20&go=1")
+          scp=BeautifulSoup(page.content,'html.parser')
+          links=scp.find_all('a',{'class':'noirot_sslien'},href=True)
+          lespages=[]
+          for link in links:
+                 if link.get('title'):
+                      lespages.append(f" {'https://dz.professeurparticulier.com'}{link['href']}")
+          result=list(set(lespages))
+          result.append("https://dz.professeurparticulier.com/marech.php?ap=9&cat=1&pp=1&pays=Alg%E9rie&nivmax=10&prox=20&go=1")
+          return result               
+# niveaux
+# https://dz.professeurparticulier.com/marech.php?ap=9&cat=1&pp=1&pays=Alg%E9rie&nivmax=10&prox=20&go=1
+# https://dz.professeurparticulier.com/marech.php?ap=9&cat=1&pp=1&pays=Alg%E9rie&nivmax=3&prox=20&go=1
+# https://dz.professeurparticulier.com/marech.php?ap=9&cat=1&pp=1&pays=Alg%E9rie&nivmax=1&prox=20&go=1
